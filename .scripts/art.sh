be85ca7c034a6995bb07717bd25f84e8facb1520
only_vip <chawhanvip@gmail.com>
2021-10-05 20:31:16 +0530

    fixed wall-change

--- .scripts/art.sh
+++ .scripts/art.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-if [ ! -f '/tmp/OTHER*' ] 
+if [ ! -f '/tmp/OTHER*' ]
 then
   rm -r $(find /tmp/OTHER*)
 else
-  echo "no files created by eyed3 creating now using eyed3"  
+  echo "no files created by eyed3 creating now using eyed3"
+fi
+if [ ! -f '/tmp/cover*' ]
+then
+  rm -r $(find /tmp/cover*)
+else
+  echo "no files created by eyed3 creating now using eyed3"
 fi
  eyeD3 --write-images=/tmp/ $HOME/Music/"$(mpc -f %file% current)" &> /dev/null
 #dunstify --icon=/tmp/OTHER.jpg "$(mpc --format '%title% \n%artist% - %album%' current)"
