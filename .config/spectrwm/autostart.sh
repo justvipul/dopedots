cbbc43c732b310968dedcc996bc53df0fb096c4d
only_vip <chawhanvip@gmail.com>
2022-03-05 09:25:02 +0530

    added awesomwm configs and spectrwm configs

---
+++ .config/spectrwm/autostart.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+$HOME/.screenlayout/display.sh
+#bash -c "sleep 2s;$HOME/.config/polybar/launch.sh" &
+#picom -b -f  &
+compton -b &
+#nitrogen --set-scaled --random &
+~/.scripts/wall_change &
+#conky -d &
+#lxappearance &
+#bash -c "sleep 10s;exec /usr/bin/apt-notifier" &
+#usb-unmounter-start &
+#bash -c "sleep 9s;stalonetray -t" &
+#bash -c "sleep 7s;tint2 -c $HOME/.config/tint2/tint2rcappsonly.tint2rc" &
+#bash -c "sleep 5s;tint2 -c tint2 -c $HOME/.config/tint2/tint2rcsystrayonly.tint2rc" &
+#swallow
+#pgrep bspswallow || bspswallow &
+#sudo pkill mpd
+#mpd
+#ntfd &
+#~/.scripts/conkystart &
+urxvtd -f -o &
+/usr/bin/lxpolkit &
